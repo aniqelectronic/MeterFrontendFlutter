8ba4fade94dbf5cfcb1365373cc03b3953d4f036
aniqelectronic <aniq_electronic@raisevest.com.my>
2026-07-03 12:36:58 +0800

    c200

--- frontend_v1/lib/im15_utils/im15_packet_builder.dart
+++ frontend_v1/lib/im15_utils/im15_packet_builder.dart
@@ -16,8 +16,7 @@ class IM15PacketBuilder {
       packet.setAll(0, dataBytes);
       packet[packet.length - 2] = ETX;
 
-      // LRC include STX also
-      int lrc = 0x02;
+      int lrc = 0x00;
       for (int i = 0; i < packet.length - 1; i++) {
         lrc ^= packet[i];
       }
