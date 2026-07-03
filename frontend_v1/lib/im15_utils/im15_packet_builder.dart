192eb10d722f4bb61f0f9eb77f3cd7efe8dc2bad
aniqelectronic <aniq_electronic@raisevest.com.my>
2026-07-03 12:00:25 +0800

    change packet

--- frontend_v1/lib/im15_utils/im15_packet_builder.dart
+++ frontend_v1/lib/im15_utils/im15_packet_builder.dart
@@ -89,9 +89,9 @@ class IM15PacketBuilder {
     // IMPORTANT:
     // Basic C200 = C200 + HostNo(2) + AccountType(1) + Amount(12)
     // Do not send additionalData unless terminal supports it.
-    // final data = 'C200$h$acc$amt';
-    final add = fixedText(additionalData, 24);
-    final data = 'C200$h$acc$amt$add';
+    final data = 'C200$h$acc$amt';
+    // final add = fixedText(additionalData, 24);
+    //final data = 'C200$h$acc$amt$add';
 
     return _asciiPacket(data, 'C200');
   }
