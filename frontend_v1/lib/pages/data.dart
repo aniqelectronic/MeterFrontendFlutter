e28a7e2dfec4f1b543eb6e457ddd2b8a33636c9a
aniqelectronic <aniq_electronic@raisevest.com.my>
2026-09-22 12:47:27 +0800

    change date

--- frontend_v1/lib/pages/data.dart
+++ frontend_v1/lib/pages/data.dart
@@ -37,7 +37,7 @@ class Data {
   static const double waterServiceFee = 1.00;
 
   //update the date the code was last updated
-  static String lastUpdatedDate = "2026-09-18 15:30:00";
+  static String lastUpdatedDate = "2026-09-22 15:30:00";
 
   //pbt which area
   static String pbtArea = "Bentong";
